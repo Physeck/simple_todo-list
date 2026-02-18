f42ddba283e5a03cfefd68be7b82b4e5d7723eca
Physeck <justintjandx@gmail.com>
2026-02-18 21:57:29 +0700

    update ui formatting, added custom logo, js

--- public/main.js
+++ public/main.js
@@ -1,6 +1,12 @@
 const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
+const deleteTaskBtn = document.getElementById("deleteTaskBtn");
+
+const typed = new Typed("#mainHeader", {
+    strings: ["Physeck"],
+    typeSpeed: 77,
+});
 
 addTaskBtn.addEventListener("click", addTask);
 taskInput.addEventListener("keydown", (event) => {
@@ -15,8 +21,36 @@ function addTask() {
     if (!taskText) return;
 
     const li = document.createElement("li");
-    li.textContent = taskText;
+    li.classList.add("task-item");
+    li.dataset.id = Date.now();
+
+    const span = document.createElement("span");
+    span.textContent = taskText;
+    span.classList.add("task-text")
+
+    const div = document.createElement("div");
+    div.classList.add("todo-container");
+
+    const markBtn = document.createElement("button");
+    markBtn.textContent = "✓"
+    markBtn.classList.add("mark-btn");
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "✕";
+    deleteBtn.classList.add("delete-btn");
+
+    div.append(markBtn, deleteBtn)
+    li.append(span, div);
     taskList.append(li);
+
+    taskInput.value = "";
+}
+
+function deleteTask() {
+    if (!taskList) return;
+
+    const li = document.createElement("li");
+    li.removeChild();
 
     taskInput.value = "";
 }
